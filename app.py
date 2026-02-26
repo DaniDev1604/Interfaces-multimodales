58edb952d2b63723508e52563c9d92fe31439e69
DaniDev1604 <danielago657@gmail.com>
2026-02-26 10:27:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,14 @@ st.image(image, caption="Banner")
 
 texto = st.text_input("Escribe algo", "Este es el texto")
 st.write("El texto escrito es", texto)
+
+st.subheader("Ahora usemos 2 columnas")
+col1,col2 = st.columns(2)
+
+with col1:
+  st.subheader("columna 1")
+  st.write("mejora la experiencia")  
+  resp = st.checkbox("De acuerdo")
+  if resp:
+    st.write("correcto")    
+    
