844933d8cfb2fdcf810597068cbfff191606cd85
DaniDev1604 <danielago657@gmail.com>
2026-02-26 10:11:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+from PIL from Image
 st.title("Desarrollado por Dani")
 
 st.header("Pàgina desarrollada como ejemplo de caso para la clase creaciòn de interfaces multimodales")
 st.write("Prueba desarrollo de backend y frontend")
-image = image.open
+image = Image.open("BannerPrueba.jpeg")
+st.image(image, caption="Banner")
