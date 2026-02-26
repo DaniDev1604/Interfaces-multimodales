14490f2f7c5889dd307dd7e01d33d296ad26a53f
DaniDev1604 <danielago657@gmail.com>
2026-02-26 10:17:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ st.header("Pàgina desarrollada como ejemplo de caso para la clase creaciòn de 
 st.write("Prueba desarrollo de backend y frontend")
 image = Image.open("BannerPrueba.jpeg")
 st.image(image, caption="Banner")
+
+texto = st.text_input("Escribe algo", "Este es el texto")
+st.write("El texto escrito es", texto)
