999f5734577485f345b3987e18556e7eb0376a8c
DaniDev1604 <danielago657@gmail.com>
2026-02-26 10:08:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 import streamlit as st
 st.title("Desarrollado por Dani")
+
+st.header("Pàgina desarrollada como ejemplo de caso para la clase creaciòn de interfaces multimodales")
+st.write("Prueba desarrollo de backend y frontend")
+image = image.open
